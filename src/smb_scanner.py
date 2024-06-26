75237c51640d13b13ac5e039782bf2694a006700
tiff <github@0x8c.org>
2024-06-25 22:33:41 -0400

    Initial code commit

--- src/smb_scanner.py
+++ src/smb_scanner.py
@@ -1,2 +1,35 @@
+import argparse
+from colorama import Fore, Style, init
+from impacket import smb, smb3, nmb, nt_errors, LOG
+import socket
+import urllib3
+import os
+from os.path import exists
+import datetime
+import sys
+
+global domain
 
 
+def options():
+    opt_parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent(
+            """Example: python3 smb_scanner.py tool -t 192.168.1.1"""
+        ),
+    )
+    requiredNamed = opt_parser.add_argument_group("required arguments")
+    requiredNamed.add_argument(
+        "-t", "--target", help="Specifies the ip address of the target.", required=True
+    )
+    global args
+    args = opt_parser.parse_args()
+    if len(sys.argv) == 1:
+        opt_parser.print_help()
+        opt_parser.exit()
+    global success, info, fail
+    success, info, fail = (
+        Fore.GREEN + Style.BRIGHT,
+        Fore.YELLOW + Style.BRIGHT,
+        Fore.RED + Style.BRIGHT,
+    )
